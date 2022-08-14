4a1186dbd75c6dcc32cc3a08a24de566df4f55d8
andrii_h <galgayand@gmail.com>
2022-08-14 21:12:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,70 @@
-import { ContactList } from './ContactList/ContactList';
-import { Section } from './Section/Section';
-import { Form } from './Form/Form';
-import { Filter } from './Filter/Filter';
-import { useSelector } from 'react-redux';
-import {
-  errorSelector,
-  loadingSelector,
-} from 'redux/contacts/contacts-selectors';
-import { Loader } from './Loader/Loader';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getCurrentUser } from 'redux/auth/auth-operations';
+import { getIsRefreshed, getToken } from 'redux/auth/auth-selectors';
+import { useEffect } from 'react';
+import { DotLoader } from 'react-spinners';
+import { AppBar } from './AppBar/AppBar';
+import { Routes, Route } from 'react-router-dom';
+import { PublicRoute } from './PublicRoute/PublicRoute';
+import { PrivateRoute } from './PrivateRoute/PrivateRoute';
+import { SingIn } from 'pages/SingIn/SingUp';
+import { Login } from 'pages/Login/Login';
+import { Contacts } from 'pages/Contacts/Contacts';
+import { NotFound } from 'pages/Page404/NotFound';
+import { Home } from './Home/Home';
+
+// const Login = lazy(() => import('pages/Login'));
+// const Contacts = lazy(() => import('pages/Contacts'));
+// const SingIn = lazy(() => import('pages/SingIn'));
+// const NotFound = lazy(() => import('pages/Page404'));
 
 export const App = () => {
-  const customError = useSelector(errorSelector);
-  const loading = useSelector(loadingSelector);
+  const token = useSelector(getToken);
+  // const isRefreshed = useSelector(getIsRefreshed);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch, token]);
+
   return (
     <>
-      <Section title="Phonebook">
-        <Form />
-      </Section>
-      <Section title="Contacts">
-        <Filter />
-        {customError && <p>{customError}</p>}
-        {!customError && <ContactList />}
-        {loading && <Loader />}
-      </Section>
+      {/* {isRefreshed ? (
+        <DotLoader />
+      ) : ( */}
+      <>
+        <AppBar />
+        <Routes>
+          <Route index path="/goit-react-hw-08-phonebook/" element={<Home />} />
+          <Route
+            path="/goit-react-hw-08-phonebook/register"
+            element={
+              <PublicRoute>
+                <SingIn />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/goit-react-hw-08-phonebook/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/goit-react-hw-08-phonebook/contacts"
+            element={
+              <PrivateRoute>
+                <Contacts />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </>
+      {/* )} */}
     </>
   );
 };
